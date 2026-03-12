0c728484e4247a3e5e8c6230bb108e2e84a1e084
mintocat <nosfir4@gmail.com>
2026-03-13 02:18:14 +0900

    Update questions.js

--- questions.js
+++ questions.js
@@ -5,4 +5,8 @@ export const allQuestions = [
     { event: "五・一五事件", year: 1932, set: 2 },
     { event: "二・二六事件", year: 1936, set: 2 },
     { event: "Y2Kと表される年", year: 2000},
+    { event: "ドラえもんの誕生年", year: 2112},
+    { event: "承久の乱", year: 1221},
+    { event: "山崎の戦い", year: 1582},
+    { event: "大化の改新", year: 645},
 ];
