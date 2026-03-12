c2008d7c0dfae1b4da92ae4d81c08b4368f2aa5f
mintocat <nosfir4@gmail.com>
2026-03-13 02:10:10 +0900

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,8 @@
+export const allQuestions = [
+    { event: "鎌倉幕府の成立", year: 1185, set: 1 },
+    { event: "頼朝が征夷大将軍となった", year: 1192, set: 1 },
+    { event: "関ヶ原の戦い", year: 1600},
+    { event: "五・一五事件", year: 1932, set: 2 },
+    { event: "二・二六事件", year: 1936, set: 2 },
+    { event: "Y2Kと表される年", year: 2000},
+];
