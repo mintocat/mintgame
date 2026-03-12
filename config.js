684db61a09cb30ae95b08622418243157e11b6e9
mintocat <nosfir4@gmail.com>
2026-03-13 03:12:01 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const questionSets = [
-    { value: "questions.js", label: "基本セット" },
-    { value: "math_set.js", label: "数学セット" },
+    { value: "questions.js", label: "基本" },
+    { value: "math_set.js", label: "数学" },
     { value: "expert.js", label: "上級者用" },
-    { value: "science.js", label: "理科セット" } // 例：こうやって増やせます
+    { value: "science.js", label: "理科" },
 ];
