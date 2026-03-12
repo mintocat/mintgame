f79b2db70a29d54777c878547fab8a04eb89fd92
mintocat <nosfir4@gmail.com>
2026-03-13 03:11:33 +0900

    Update math_set.js

--- math_set.js
+++ math_set.js
@@ -1,4 +1,5 @@
 export const allQuestions = [
     { event: "$\\sqrt{64} \\times 100$ を計算せよ", year: 800 },
     { event: "黄金比 $\\phi$ の近似値 $\\times 1000$", year: 1618 },
+    { event: "$1 + \\cfrac{1}{2 + \\cfrac{1}{3 + \\cfrac{1}{4 + \\cfrac{1}{5 + \\dots}}}}$ の近似値 $\\times 1000$", year: 1433 },
 ];
